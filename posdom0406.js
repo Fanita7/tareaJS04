b2b2f6d54fe41203e85e868db7ad11b29610679f
Stephanie Chávez <lschavezbarajas@gmail.com>
2024-11-05 15:43:15 -0600

    Add files via upload

---
+++ posdom0406.js
@@ -0,0 +1,25 @@
+/*Positive dominance in Array
+ 
+
+Write a function to determine if an array is positively dominant.
+An array is positively dominant when the majority of its elements are positive.
+ 
+
+Example:
+- `positiveDom([-1, -3, -5, 4, 6767])` should return `false`.
+*/
+
+function positiveDom(numeros){
+    let numeroPositivo = 0;
+    let numeroNegativo = 0;
+
+    numeros.forEach((element) => {
+        if(element < 0) numeroNegativo++;
+        else numeroPositivo++;
+    })
+    if (numeroPositivo > numeroNegativo) return true;
+    else return false;
+}
+
+console.log(positiveDom([1, -3, -5, 4, 6767]));
+console.log(positiveDom([1, 2, -4, 32]));
