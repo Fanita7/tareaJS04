b212543ff89c590d7a469608020a734ce89f0260
Stephanie Chávez <lschavezbarajas@gmail.com>
2024-11-05 14:07:38 -0600

    Add files via upload

---
+++ arraym0405.js
@@ -0,0 +1,27 @@
+/*Array of Multiples
+ 
+
+Create a function that takes two parameters (number, length) and returns an array of length containing multiples of the number.
+ 
+
+Examples:
+- `arrayMultiplos(2, 10)` should return `[2, 4, 6, 8, 10, 12, 14, 16, 18, 20]`.
+- `arrayMultiplos(17, 6)` should return `[17, 34, 51, 68, 85, 102]`.
+*/
+
+function arrayMultiplos(numero, arrLength){
+    let paso = 1;
+    let numeroInicial = numero;
+    let respuesta = [];
+
+    while(paso < arrLength + 1) {
+        numero = numeroInicial * paso;
+        respuesta.push(numero);
+
+        paso++;
+    }
+    return respuesta;
+}
+
+console.log(arrayMultiplos(2, 10));
+console.log(arrayMultiplos(17, 6));
